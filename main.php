10526bd14bd01c36107f63d248975fb79df72477
Party4Bread <solsam10@naver.com>
2018-07-21 06:40:43 +0900

    Add main
    asdf

---
+++ main.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+</head>
+
+<body>
+    <div class="mobcontainer">
+
+        <div class="titlecontainer">
+            <img src="assets/image/lgo.png" class="logoimage">
+        </div>
+
+        <div class="inputcontainer">
+           <p> <img src="assets/image/location.svg" style="display: inline;" width="5%"><b>서울특별시</b>에서의 Brief</p>
+        </div>
+        <div id="slider">
+            <figure>
+            <img src="assets/image/slider_img_1.png">
+            <img src="assets/image/slider_img_2.png">
+            <img src="assets/image/slider_img_3.png">
+            </figure>
+        </div>
+       
+        <br>
+        <a class="btn" href="pointmall.html" style="float:left;">적립한 포인트<p class="num" style="font-size:8.5rem; margin-top:auto; margin-bottom: auto; margin-left:auto; margin-right:auto; font-family:Abel; padding:15% 0;">120</p></a>
+        <a class="btn" href="stats.html" style="float:right;">평가한 Brief<p class="num" style="font-size:8.5rem; margin-top:auto; margin-bottom: auto; margin-left:auto; margin-right:auto; font-family:Abel; padding:15% 0;">54</p></a>
+
+    </div>
+
+</body>
+
+</html>
