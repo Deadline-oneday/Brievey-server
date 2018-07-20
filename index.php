fd4a50a16b1abc197c2ef2c1b314d5f3b8cce8c6
Party4Bread <solsam10@naver.com>
2018-07-21 05:49:09 +0900

    merge idx and lgin

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
- echo "Hello world";
+session_start(); // 세션
 ?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/earlyaccess/notosanskr.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+    <meta charset="utf8">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="namepan">Brievey!</h1>
+        <img class="mlogo" src="assets/image/lgo.png"></img>
+        <p class="intro">
+            <span>여러분이 사는 도시,</span><br>
+            <b>당신이 곧 시장입니다.</b>
+        </p>
+        <a class="start" href="<?=($_SESSION['id']==null)?"login.php":"main.php"?>">시작하기</a>
+    </div>
+    <script src="assets/js/index.js"></script>
+</body>
+
+</html>
--- login.php
+++ login.php
@@ -3,6 +3,44 @@ session_start(); // 세션
 if($_SESSION['id']==null) { // 로그인 하지 않았다면
 ?>
 
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
+</head>
+
+<body>
+    <div class="mobcontainer">
+        <div class="titlecontainer">
+            <img src="assets/image/lgo.png" class="logoimage">
+            <b class="logotext">로그인</b>
+        </div>
+
+        <form id="lginform" action="logincheck.php" method="post">
+            <div class="inputcontainer">
+                <p>이메일</p>
+                <input type="email" name="id">
+            </div>
+            <div class="inputcontainer">
+                <p>비밀번호</p>
+                <input type="password" name="pwd">
+            </div>
+            <a href="#" class="login" onclick="document.getElementById('lginform').submit();">로그인</a>
+        </form>
+       
+        <a class="register" href="signup.php">계정 만들기</a>
+
+        <a class="forgetpass" href="#">비밀번호를 잊어버리셨나요?</a>
+
+    </div>
+
+</body>
+
+</html>
+<!--
 <center><br><br><br>
 <form name="login_form" action="logincheck.php" method="post"> 
    ID : <input type="text" name="id"><br> 
@@ -14,7 +52,7 @@ if($_SESSION['id']==null) { // 로그인 하지 않았다면
 	<input type="submit" name="register" value="register">
 	</form>
 </center>
-
+-->
 <?php
 }else{ // 로그인 했다면
    echo "<center><br><br><br>";
