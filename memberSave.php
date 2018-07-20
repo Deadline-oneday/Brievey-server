70d872ede07e4b5bf6aff31479a35b49036e0ce7
kangsecu <kangsecu@gmail.com>
2018-07-21 06:23:49 +0900

    9차 수정

--- memberSave.php
+++ memberSave.php
@@ -4,7 +4,12 @@
  $id=$_POST['id'];
  $password=md5($_POST['pwd']);
  $password2=$_POST['pwd2'];
- $sql = "insert into account_info (id, pwd) values ('$id','$password')";
+ $name= 1;
+ $address=1;
+ $sex=1;
+ $birthDay=1;
+ $email=1;
+ $sql = "insert into account_info (id, pwd, name,addr,sex,birthDay,email) values ('$id','$password','$name','$address','$sex',$birthDay,'$email')";
 echo $sql;
  if($con->query($sql)){
   echo 'success inserting';
