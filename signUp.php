e80fd01439849093c7c071483c648ea92972aed4
Party4Bread <solsam10@naver.com>
2018-07-21 06:34:51 +0900

    fix signup add stats

--- signUp.php
+++ signUp.php
@@ -1,3 +1,4 @@
+<!--
 <!doctype html>
 <html>
 <head>
@@ -24,4 +25,47 @@
  <input type=submit value="submit"><input type=reset value="rewrite">
 </form>
 </body>
+</html>
+-->
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
+
+</head>
+
+<body>
+    <div class="mobcontainer">
+
+        <div class="titlecontainer">
+            <img src="assets/image/lgo.png" class="logoimage">
+            <b class="logotext">회원 가입</b>
+        </div>
+
+        <form id="rgstform" action="memberSave.php" method="post">
+        <div class="inputcontainer">
+            <p>이메일</p>
+            <input type="email" name="id">
+        </div>
+
+        <div class="inputcontainer">
+            <p>비밀번호</p>
+            <input type="password" name="pwd">
+        </div>
+
+        <div class="inputcontainer">
+            <p>비밀번호 확인</p>
+            <input type="password" name="pwd2">
+        </div>
+
+        <a href="#" class="login" onclick="document.getElementById('lginform').submit();">회원 가입</a>
+        </form>
+
+    </div>
+
+</body>
+
 </html>
