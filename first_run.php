f3eb1302d47eaf24dd89f5fa2f1dec35b96edf3b
Party4Bread <solsam10@naver.com>
2018-07-21 06:43:12 +0900

    add fr

---
+++ first_run.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/signin.css">
+    <link rel="stylesheet" href="assets/css/first-run.css">
+    <link rel="stylesheet" href="assets/css/survey.css">
+
+</head>
+
+<body>
+    <div class="mobcontainer">
+        <div class="logotext">
+            <b class="logotext">시작하기</b>
+        </div>
+        <div class="subtext">
+            <br> 관심 분야를 선택하세요<br>
+            <div class="left">
+                <p class="button_normal">교육</p>
+                <p class="button_normal">교통</p>
+                <p class="button_normal">일자리</p>
+                <p class="button_normal">환경</p>
+            </div>
+            <div class="right">
+                <p class="button_normal">토지사업</p>
+                <p class="button_normal">복지</p>
+                <p class="button_normal">도시안정</p>
+                <p class="button_normal">문화관광</p>
+            </div>
+            <hr>
+            <br>
+        </div>
+
+        <div class="subtext">살고 계신 지역을 선택하세요<br>
+
+            <select class="noborder" name="" id="">
+                <option value="seoul">서울특별시</option>
+            </select>
+            <a class="login" href="main.php">다음</a>
+        </div>
+    </div>
+
+    <script src="assets/js/jquery.min.slim.js"></script>
+    <script src="assets/js/first-run.js"></script>
+</body>
+
+</html>
